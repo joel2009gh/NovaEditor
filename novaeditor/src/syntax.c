2e03930cdfc43739434e563591e72aecd80ce513
joel2009gh <joel2009github@gmail.com>
2025-04-20 16:33:04 +0200

    Update syntax.c

--- novaeditor/src/syntax.c
+++ novaeditor/src/syntax.c
@@ -7,7 +7,7 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdlib.h>
-#include <backup.h>
+#include "backup.h"
 char *C_HL_extensions[] = { ".c", ".h", ".cpp", NULL };
 char *C_HL_keywords[] = {
     "switch", "if", "while", "for", "break", "continue", "return", "else",
