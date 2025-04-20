1773a1263318a23d2f1ebcefb4f3d135cf827aff
joel2009gh <joel2009github@gmail.com>
2025-04-20 16:29:00 +0200

    Update main.c

--- novaeditor/src/main.c
+++ novaeditor/src/main.c
@@ -5,22 +5,24 @@
 #include "buffer.h"
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <backup.h>
 int main(int argc, char *argv[]) {
     enableRawMode();
     initEditor();
-    
     if (argc >= 2) {
-        editorOpen(argv[1]);
+        if (argc >= 3 && strcmp(argv[2], "--restore") == 0) {
+            editorOpen(argv[1]);
+            editorRestoreBackup(argv[1]);
+        } else {
+            editorOpen(argv[1]);
+        }
     }
-    
     editorSetStatusMessage("HELP: Ctrl-S = save | Ctrl-Q = quit | Ctrl-F = find");
-    
     while (1) {
         editorRefreshScreen();
         editorProcessKeypress();
     }
-    
     return 0;
 }
+
 
