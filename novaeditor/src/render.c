d77b40e4fe5d09ab56ef7ca20a4d71c58806dd2b
joel2009gh <joel2009github@gmail.com>
2025-04-20 16:29:13 +0200

    Update render.c

--- novaeditor/src/render.c
+++ novaeditor/src/render.c
@@ -8,7 +8,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <ctype.h>
-
+#include <backup.h>
 void abAppend(struct abuf *ab, const char *s, int len) {
     char *new = realloc(ab->b, ab->len + len);
     
